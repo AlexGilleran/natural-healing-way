e4f0a163de8b9fe780bcd9c5fee462cd55be6ef9
Alex Gilleran <alex@alexgilleran.com>
2020-03-01 23:06:26 +1100

    Made the form real.

--- components/contact-us-form.tsx
+++ components/contact-us-form.tsx
@@ -3,15 +3,18 @@ export default function ContactUsForm() {
     <div className="my-5">
       <h2 className="font-bold text-lg">Contact Us</h2>
       <form
+        method="POST"
         name="contact-us"
         className="bg-nhw-contact-us-bg block p-2 rounded mt-2"
         data-netlify="true"
+        action="/contact-success"
       >
         <input type="hidden" name="form-name" value="contact-us" />
         <label className="m-2 block">
           Name (required)
           <input
             type="text"
+            name="name"
             className="block rounded mb-3 mt-1 w-full p-1"
             required
           ></input>
@@ -21,6 +24,7 @@ export default function ContactUsForm() {
           Email (required)
           <input
             type="email"
+            name="email"
             className="block rounded mb-3 mt-1 w-full p-1"
             required
           ></input>
@@ -30,6 +34,7 @@ export default function ContactUsForm() {
           Mobile number (required)
           <input
             type="phone"
+            name="phone"
             className="block rounded mb-3 mt-1 w-full p-1"
             required
           ></input>
@@ -40,15 +45,17 @@ export default function ContactUsForm() {
           <textarea
             className="block rounded mb-3 mt-1 w-full p-1 resize-none"
             required
+            name="query"
             rows={7}
           ></textarea>
         </label>
 
-        <input
+        <button
           type="submit"
           className="inline-block p-2 rounded m-2 border border-gray-500 border-solid hover:bg-gray-400 hover:cursor-pointer"
-          value="Send"
-        />
+        >
+          Send
+        </button>
       </form>
     </div>
   );
